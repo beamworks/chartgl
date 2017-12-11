0f0e56c7f67660954ff8555b303262618a508cc2
Nick Matantsev <nick.matantsev@gmail.com>
2017-12-10 20:22:34 -0500

    Demo chart reset

--- src/DemoStage.jsx
+++ src/DemoStage.jsx
@@ -9,19 +9,51 @@ class DemoStage extends React.PureComponent {
     constructor() {
         super();
 
+        this.state = {
+            version: 0
+        };
     }
 
     render() {
         document.body.style.background = palette[0];
 
-        return <Chart
-            width={640}
-            height={480}
-            baseColor={palette[3]}
-            secondaryColor={palette[4]}
-            highlightColor={palette[2]}
-            labelColor={palette[1]}
-        />;
+        return <div style={{
+            display: 'inline-block'
+        }}>
+            <button
+                type="button"
+                style={{
+                    display: 'block',
+                    margin: '0 auto 60px',
+                    width: '200px',
+                    height: '60px',
+                    border: '0',
+                    padding: '0 0 5px',
+                    borderRadius: '3px',
+                    background: 'rgba(255, 255, 255, 0.3)',
+                    color: '#fff',
+                    fontFamily: 'Michroma, Arial, sans-serif',
+                    fontSize: '24px',
+                    outline: 'none', // @todo this breaks accessibility
+                    cursor: 'pointer'
+                }}
+                onClick={() => {
+                    this.setState({
+                        version: this.state.version + 1
+                    });
+                }}
+            >Reset</button>
+
+            <Chart
+                key={this.state.version}
+                width={640}
+                height={480}
+                baseColor={palette[3]}
+                secondaryColor={palette[4]}
+                highlightColor={palette[2]}
+                labelColor={palette[1]}
+            />
+        </div>;
     }
 }
 
